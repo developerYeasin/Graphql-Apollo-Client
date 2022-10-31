c265d52d0b0002e019ea17653354a135a33f78e2
developerYeasin <yea13sin@gmail.com>
2022-10-31 22:15:02 +0600

    ..

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  // uri: 'http://localhost:5000/products',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client} >
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
